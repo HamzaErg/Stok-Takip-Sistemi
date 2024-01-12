0a2123be5adae4e71b800255755bbd6053102cb7
Hamza <hamzakaan003@gmail.com>
2024-01-13 00:20:51 +0300

    Stok Takip Sistemi
    Bu projede programlama dili olarak C# ve veri tabanı tablolarının oluşturulmasında Sqlide kullanılmıştır.
    Projeyi çalıştırdığınızda yönetici olarak giriş yapmak isateseniz ad ve şifre yerine "yönetici" , personel ile giriş yapmak için ad ve şifre bölümüne "personel" yazmanız gerekli.
    Yönetici ile giriş yapıldığında ürünler ve çalışanlar tablosunun verilerine ekleme , güncelleme ve silme işi yaparken personel ile giriş yapıldığında sadece ürün verilerine müdahale edilebilmektedir.

---
+++ Stok_Takip_sistemi/Stok_Takip_sistemi/Forms/frmAnaSayfaPersonel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stok_Takip_sistemi.Forms
+{
+    public partial class frmAnaSayfaPersonel : Form
+    {
+        public frmAnaSayfaPersonel()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUrunler_Click(object sender, EventArgs e)
+        {
+            frmUrunler frm = new frmUrunler();
+            frm.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Stok_Takip_sistemi/Stok_Takip_sistemi/frmGiris.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Stok_Takip_sistemi
+{
+    partial class frmGiris
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(14, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Stok Takip Giriş Ekranı";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(79, 83);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(166, 22);
+            this.txtAd.TabIndex = 1;
+            this.txtAd.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Şifre:";
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(79, 117);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(166, 22);
+            this.txtSifre.TabIndex = 4;
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.Location = new System.Drawing.Point(16, 158);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(231, 36);
+            this.btnGiris.TabIndex = 5;
+            this.btnGiris.Text = "Giriş";
+            this.btnGiris.UseVisualStyleBackColor = true;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.IndianRed;
+            this.button1.Location = new System.Drawing.Point(167, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 30);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Çıkış";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmGiris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(258, 303);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmGiris";
+            this.Text = "Stok Takip Giriş Sayfası";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
